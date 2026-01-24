b6b3c1bbfd019a1cce2f097ab49cd05b24c833a9
Ronan F․ <ronan.fesselier@etik.com>
2026-01-24 21:44:41 +0100

    chore(deps): bump nfx-json from 1.0.0 to 1.0.1

--- cmake/nfxSerializationDependencies.cmake
+++ cmake/nfxSerializationDependencies.cmake
@@ -18,7 +18,7 @@ set(CMAKE_FIND_QUIETLY      ON     )
 # Dependency versions
 #----------------------------------------------
 
-set(NFX_DEPS_NFX_JSON_VERSION "1.0.0")
+set(NFX_DEPS_NFX_JSON_VERSION "1.0.1")
 
 #----------------------------------------------
 # FetchContent dependencies
